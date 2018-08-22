59834ed840c806ab13fde10ca0366758d34f14d0
Milan222 <milan15697@gmail.com>
2018-08-22 23:14:05 +0530

    EVEN NO

---
+++ even.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class even {
+  public static void main(String[] args) {
+
+    int num;
+Scanner scan= new Scanner(System.in);
+num=scan.nextInt();
+
+for (int i=0;i<=num;i++)
+{
+ if (i%2==0)
+  System.out.println(i);
+
+
+}
+  }
+}
